e48ced5ad1d7ec065870980b34451274ce62e4ec
fatma <fatmahassansultan97@gmail.com>
2024-09-01 22:34:51 +0300

    added some changes

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,15 +1,20 @@
 // src/App.jsx
 import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
+import FormikRegistrationForm from './components/FormikRegistrationForm';
 
 function App() {
   return (
     <div>
       <h1>User Registration Form</h1>
       <RegistrationForm />
+      <h1>User Registration Form (Formik)</h1>
+      <FormikRegistrationForm />
     </div>
   );
 }
+
+export default App;
 
 
 
