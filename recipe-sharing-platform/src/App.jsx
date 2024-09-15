38eb1f1fec6920055fc4e87dc1cd7bfeae357b19
fatma <fatmahassansultan97@gmail.com>
2024-09-16 01:58:15 +0300

    added a new folder

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-blue-500">Recipe Sharing Platform</h1>
+      <p>Welcome to the Recipe Sharing Platform!</p>
+    </div>
+  );
+}
+
+export default App;
